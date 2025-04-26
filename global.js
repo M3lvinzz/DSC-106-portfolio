002bb1bde0aefba57a50629283ddba09d3c9b27b
M3lvinzz <melvinsasylum@gmail.com>
2025-04-25 19:48:24 -0700

    lab4

--- global.js
+++ global.js
@@ -86,3 +86,47 @@ form?.addEventListener('submit', function (event) {
   const url = `mailto:${form.action}?${params.join('&')}`;
   location.href = url;
 });
+
+export async function fetchJSON(url) {
+    try {
+      // Fetch the JSON file from the given URL
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error(`Failed to fetch projects: ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching or parsing JSON data:', error);
+    }
+  }
+
+  export function renderProjects(projects, containerElement, headingLevel = 'h2') {
+    containerElement.innerHTML = '';
+  
+    for (let i = 0; i < projects.length; i++) {
+      const article = document.createElement('article');
+  
+      const img = document.createElement('img');
+      img.src = projects[i].image;
+      img.alt = projects[i].title;
+  
+      const heading = document.createElement(headingLevel);
+      heading.textContent = projects[i].title;
+  
+      const description = document.createElement('p');
+      description.textContent = projects[i].description;
+  
+      article.appendChild(heading);
+      article.appendChild(img);
+      article.appendChild(description);
+  
+      containerElement.appendChild(article);
+    }
+  }
+  
+  export async function fetchGitHubData(username) {
+    return fetchJSON(`https://api.github.com/users/${username}`);
+  }
